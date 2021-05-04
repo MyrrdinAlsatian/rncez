defabacfaeba817b5673d491d0e9ba1a5392d77d
jb <stephan.jeanba@gmail.com>
2021-05-04 17:15:53 +0200

    start README + undo refactoring save for later

--- src/utils/gnrcComponentUtils.js
+++ src/utils/gnrcComponentUtils.js
@@ -4,7 +4,7 @@ const replace = require('replace');
 const { camelCase } = require('lodash');
 const { existsSync, outputFileSync, readFileSync } = require('fs-extra');
 
-const componentTemplateGenerator = require('../generator/componentTemplateGenerator')
+// const componentTemplateGenerator = require('../generator/componentTemplateGenerator')
 
 const componentCssTemplate = require('../templates/component/componentCssTemplate');
 const componentLazyTemplate = require('../templates/component/componentLazyTemplate');
@@ -80,64 +80,64 @@ Please make sure you're pointing to the right custom template path in your rncez
 
 // -- Template for the component
 
-// function componentTemplateGenerator({ cmd, componentName, cliConfigFile }) {
-//   const { cssPreprocessor, testLibrary, usesCssModule, usesTypeScript } = cliConfigFile;
-//   const { customTemplates } = cliConfigFile.component[cmd.type];
-//   let template = null;
-//   let filename = null;
+function componentTemplateGenerator({ cmd, componentName, cliConfigFile }) {
+  const { cssPreprocessor, testLibrary, usesCssModule, usesTypeScript } = cliConfigFile;
+  const { customTemplates } = cliConfigFile.component[cmd.type];
+  let template = null;
+  let filename = null;
 
-//   // Check for a custom component template.
+  // Check for a custom component template.
 
-//   if (customTemplates && customTemplates.component) {
-//     // --- Load and use the custom component template
+  if (customTemplates && customTemplates.component) {
+    // --- Load and use the custom component template
 
-//     const { template: customTemplate, filename: customTemplateFilename } = getCustomTemplate(
-//       componentName,
-//       customTemplates.component
-//     );
+    const { template: customTemplate, filename: customTemplateFilename } = getCustomTemplate(
+      componentName,
+      customTemplates.component
+    );
 
-//     template = customTemplate;
-//     filename = customTemplateFilename;
-//   } else {
-//     // --- Else use rncez built-in component template
+    template = customTemplate;
+    filename = customTemplateFilename;
+  } else {
+    // --- Else use rncez built-in component template
 
-//     template = usesTypeScript ? componentTsTemplate : componentJsTemplate;
-//     filename = usesTypeScript ? `${componentName}.tsx` : `${componentName}.js`;
+    template = usesTypeScript ? componentTsTemplate : componentJsTemplate;
+    filename = usesTypeScript ? `${componentName}.tsx` : `${componentName}.js`;
 
-//     // --- If test library is not Testing Library or if withTest is false. Remove data-testid from template
+    // --- If test library is not Testing Library or if withTest is false. Remove data-testid from template
 
-//     if (testLibrary !== 'Testing Library' || !cmd.withTest) {
-//       template = template.replace(` data-testid="TemplateName"`, '');
-//     }
+    if (testLibrary !== 'Testing Library' || !cmd.withTest) {
+      template = template.replace(` data-testid="TemplateName"`, '');
+    }
 
-//     // --- If it has a corresponding stylesheet
+    // --- If it has a corresponding stylesheet
 
-//     if (cmd.withStyle) {
-//       const module = usesCssModule ? '.module' : '';
-//       const cssPath = `${componentName}${module}.${cssPreprocessor}`;
+    if (cmd.withStyle) {
+      const module = usesCssModule ? '.module' : '';
+      const cssPath = `${componentName}${module}.${cssPreprocessor}`;
 
-//       // --- If the css module is true make sure to update the template accordingly
+      // --- If the css module is true make sure to update the template accordingly
 
-//       if (module.length) {
-//         template = template.replace(`'./TemplateName.module.css'`, `'./${cssPath}'`);
-//       } else {
-//         template = template.replace(`{styles.TemplateName}`, `"${componentName}"`);
-//         template = template.replace(`styles from './TemplateName.module.css'`, `'./${cssPath}'`);
-//       }
-//     } else {
-//       // --- If no stylesheet, remove className attribute and style import from jsTemplate
+      if (module.length) {
+        template = template.replace(`'./TemplateName.module.css'`, `'./${cssPath}'`);
+      } else {
+        template = template.replace(`{styles.TemplateName}`, `"${componentName}"`);
+        template = template.replace(`styles from './TemplateName.module.css'`, `'./${cssPath}'`);
+      }
+    } else {
+      // --- If no stylesheet, remove className attribute and style import from jsTemplate
 
-//       template = template.replace(` className={styles.TemplateName}`, '');
-//       template = template.replace(`import styles from './TemplateName.module.css';`, '');
-//     }
-//   }
+      template = template.replace(` className={styles.TemplateName}`, '');
+      template = template.replace(`import styles from './TemplateName.module.css';`, '');
+    }
+  }
 
-//   return {
-//     componentPath: `${cmd.path}/${componentName}/${filename}`,
-//     filename,
-//     template,
-//   };
-// }
+  return {
+    componentPath: `${cmd.path}/${componentName}/${filename}`,
+    filename,
+    template,
+  };
+}
 
 // -- Template for the style files
 

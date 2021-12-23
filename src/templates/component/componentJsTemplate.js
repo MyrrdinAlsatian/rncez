4876d07c033920162bc1d340267c6c7e6367ad15
Stephan Jean-Baptiste <stephan.jeanba@gmail.com>
2021-12-23 16:31:28 +0100

    fix  state declaration in basic renderer component
    the basic component is a stateless component so it should not declare a state

--- src/templates/component/componentJsTemplate.js
+++ src/templates/component/componentJsTemplate.js
@@ -1,12 +1,7 @@
-module.exports =`import React, { useState } from 'react';
+module.exports =`import React from 'react';
 import styles from './TemplateName.module.scss';
 
 const TemplateName = () => (
-
-  const [state, setState] = useState({
-
-  });
-
   <div className={styles.TemplateName} data-testid="TemplateName">
     TemplateName Component
   </div>

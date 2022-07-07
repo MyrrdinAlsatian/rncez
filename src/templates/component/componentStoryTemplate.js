2848f3443af59f9c1281224a7d8009eb0756d858
Jean-Baptiste Stephan <jean-baptiste.stephan@ircad.fr>
2022-07-07 11:18:47 +0200

    [👔 metier](Storybook): update of the storybook template

--- src/templates/component/componentStoryTemplate.js
+++ src/templates/component/componentStoryTemplate.js
@@ -1,7 +1,17 @@
 module.exports = `/* eslint-disable */
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import TemplateName from './TemplateName';
+import TemplateName from '../TemplateName';
 
-storiesOf('TemplateName', module).add('default', () => <TemplateName />);
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'TemplateName',
+    component: TemplateName,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    // argTypes: {
+    // },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = args => <TemplateName {...args} />;
+
 `;

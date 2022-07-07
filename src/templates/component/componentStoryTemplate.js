b0baa6af6623083430f1d9578ed9ccf849dd4e83
Jean-Baptiste Stephan <jean-baptiste.stephan@ircad.fr>
2022-07-07 11:26:00 +0200

    [🔀 merge]: merge

--- src/templates/component/componentStoryTemplate.js
+++ src/templates/component/componentStoryTemplate.js
@@ -1,6 +1,5 @@
 module.exports = `/* eslint-disable */
 import React from 'react';
-<<<<<<< HEAD
 import TemplateName from '../TemplateName';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -14,9 +13,5 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = args => <TemplateName {...args} />;
-=======
-import { storiesOf } from '@storybook/react';
-import TemplateName from '../TemplateName';
->>>>>>> 66601a69561204c53a995f78489b7682a8438e79
 
 `;
